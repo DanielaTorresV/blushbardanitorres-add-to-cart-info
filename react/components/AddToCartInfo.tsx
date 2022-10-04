3925b81992897647c10d4f93293a53f075028b0a
Daniela <dannytorres0211@gmail.com>
2022-10-04 18:41:47 -0500

    feat: create components to Product Group, Button Group and Totalizers

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,8 +1,26 @@
-import React from "react"
+import React from "react";
+import { useProduct } from "vtex.product-context";
+import { useOrderForm } from "vtex.order-manager/OrderForm";
+import ProductGroup from "./ProductGroup";
+import Totalizers from "./Totalizers";
+import ButtonGroup from "./ButtonGroup";
 
 const AddToCartInfo = () => {
+  const productInfo = useProduct();
+  const { orderForm: {
+    items,
+    totalizers
+  }} = useOrderForm();
+
+  console.log("Product Info:", productInfo);
+  console.log("Order Form Info:", items, totalizers);
+
   return(
-  <div>Aquí Renderizaremos nuestra info</div>
+    <>
+      <ProductGroup />
+      <Totalizers />
+      <ButtonGroup />
+    </>
   )
 }
 
---
+++ react/components/Totalizers.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Totalizers = () => {
+  return(
+    <div>Totalizers</div>
+  )
+}
+
+export default Totalizers
